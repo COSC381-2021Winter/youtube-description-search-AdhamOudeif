c69d9857befeca4e64ff139aeae0f525d7f1ad1f
AdhamOudeif <adhamoudeif@gmail.com>
2021-04-12 07:08:53 +0000

    retrieve a complete description for every video in search result

--- flaskr/youtube.py
+++ flaskr/youtube.py
@@ -9,15 +9,34 @@ cse_id = config.cse_id
 service = build("youtube", "v3", developerKey=api_key)
 
 def search(query_term, max_page_count):
+    def get_video_info(video_id):
+        result = service.videos().list(part='snippet', id=video_id).execute()
+        try:
+            return result['items'][0]
+        except:
+            return None
+    
+    def get_video_list(search_results):
+        video_list = []
+        for item in search_results['items']:
+            video_id = item['id']['videoId']
+            video_info = get_video_info(video_id)
+            if video_info:
+                video_list.append(video_info)
+        
+        return video_list
+
     pageCounter=0
     resultList=[]
     results = service.search().list(part="snippet", type="video", maxResults=50, q=query_term).execute()
-    resultList.extend(results['items'])
+    video_list = get_video_list(results)
+    resultList.extend(video_list)
     nextPage=results.get('nextPageToken',None)
 
     while nextPage and (pageCounter < max_page_count):
         results = service.search().list(part="snippet", type="video", maxResults=50, pageToken=nextPage, q=query_term).execute()
-        resultList.extend(results['items'])
+        video_list = get_video_list(results)
+        resultList.extend(video_list)
 
         pageCounter = pageCounter + 1
         nextPage=results.get('nextPageToken',None)
